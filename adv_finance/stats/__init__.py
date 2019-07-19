142a20d3c227aa0fb729d6843115317261246340
Unknown <changwan.jang@gmail.com>
2019-07-20 03:17:36 +0900

    add half_life, hurst_exponent

--- adv_finance/stats/__init__.py
+++ adv_finance/stats/__init__.py
@@ -1,5 +1,9 @@
+
+
 import pandas as pd
 import numpy as np
+
+from numpy import log, polyfit, sqrt, std, subtract
 
 
 def get_daily_vol(close, lookback=100):
@@ -27,3 +31,30 @@ def get_daily_vol(close, lookback=100):
     df0 = close.loc[df0.index] / close.loc[df0.values].values - 1 # daily returns
     df0 = df0.ewm(span=lookback).std()
     return df0
+
+
+def get_half_life(ts):
+    lagged_ts = ts.shift(1).fillna(method='ffill')
+    delta = ts - lagged_ts
+    beta = np.polyfit(lagged_ts.dropna(), delta.dropna(), 1)[0]
+    half_life = (-1 * np.log(2) / beta)
+
+    return half_life
+
+
+def get_hurst_exponent(ts):
+
+    """Returns the Hurst Exponent of the time series vector ts"""
+    # Create the range of lag values
+    lags = range(2, 100)
+
+    # Calculate the array of the variances of the lagged differences
+    # Here it calculates the variances, but why it uses
+    # standard deviation and then make a root of it?
+    tau = [sqrt(std(subtract(ts[lag:], ts[:-lag]))) for lag in lags]
+
+    # Use a linear fit to estimate the Hurst Exponent
+    poly = polyfit(log(lags), log(tau), 1)
+
+    # Return the Hurst exponent from the polyfit output
+    return poly[0]*2.0
